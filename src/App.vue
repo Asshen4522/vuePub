e9468c3c808c43913c6771321b8645e9e01b592e
Асхен <Asshen4522@yandex.ru>
2025-03-10 09:21:30 +0300

    send

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+
+const local = reactive({
+  stroka : "Переменная из строки"
+})
+
+</script>
+
+<template>
+  <HelloWorld :msg="local.stroka" />
+  <HelloWorld msg="local.stroka" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref,reactive } from 'vue'
+
+const props = defineProps({
+  msg : String
+})
+
+const count = ref(0)
+const local = reactive({
+  per1: 6,
+  per2: 2
+})
+
+function SomeFunction() {
+  count.value--
+  local.per2 += local.per1
+}
+</script>
+
+<template>
+  <h1>{{ local.per2 }}</h1>
+  <h1>{{ props.msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="SomeFunction">count is {{ count }}</button>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
